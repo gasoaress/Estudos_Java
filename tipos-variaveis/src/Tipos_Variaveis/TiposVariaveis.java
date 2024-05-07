6635cc4606f3b1174b65e9a830f860c038ea978a
gasoaress <gabrielsoaresramos05@gmail.com>
2024-05-07 12:18:56 -0300

    Package

---
+++ tipos-variaveis/src/Tipos_Variaveis/TiposVariaveis.java
@@ -0,0 +1,16 @@
+package Tipos_Variaveis;
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 1300.5;
+
+        short numeroCurto = 1;
+        int numeroNormal = 5000;
+
+        /*
+         * NÃO É POSSÍVEL FAZER A SEGUINTE OPERAÇAO POIS TEMOS UM SHORT RECEBENDO UM INT
+         * short numeroCurto2 = numeroNormal;
+         * PODE-SE FAZER O TYPE CAST PARA SOLUCIONAR
+         * short numeroCurto2 = (short) numeroNormal
+         */
+    }
+}
---
+++ tipos-variaveis/src/Tipos_Variaveis/VariaveisConstantes.java
@@ -0,0 +1,6 @@
+package Tipos_Variaveis;
+public class VariaveisConstantes {
+    public static void main(String[] args) throws Exception {
+        final double VALOR_DE_PI = 3.14;
+    }
+}
