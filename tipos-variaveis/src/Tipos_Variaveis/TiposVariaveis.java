6f263d88fd9bb48f0fd027beec5b7a955c2a7165
gasoaress <gabrielsoaresramos05@gmail.com>
2024-05-07 14:27:57 -0300

    Prints

--- tipos-variaveis/src/Tipos_Variaveis/TiposVariaveis.java
+++ tipos-variaveis/src/Tipos_Variaveis/TiposVariaveis.java
@@ -6,6 +6,10 @@ public class TiposVariaveis {
         short numeroCurto = 1;
         int numeroNormal = 5000;
 
+        System.out.println(salarioMinimo);
+        System.out.println(numeroCurto);
+        System.out.println(numeroNormal);
+
         /*
          * NÃO É POSSÍVEL FAZER A SEGUINTE OPERAÇAO POIS TEMOS UM SHORT RECEBENDO UM INT
          * short numeroCurto2 = numeroNormal;
--- tipos-variaveis/src/Tipos_Variaveis/VariaveisConstantes.java
+++ tipos-variaveis/src/Tipos_Variaveis/VariaveisConstantes.java
@@ -2,5 +2,7 @@ package Tipos_Variaveis;
 public class VariaveisConstantes {
     public static void main(String[] args) throws Exception {
         final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
     }
 }
